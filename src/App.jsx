76ad807cd4ddfb7a6b6e8fe744185a6033998a47
Nurul Azim Sakib <sakibruns@gmail.com>
2025-08-09 02:14:16 +0600

    updated: entire structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import CreateSession from "./pages/CreateSession";
-
-
+import Sender from "./pages/Sender";
+import Receiver from "./pages/Receiver";
+import "./index.css";
 
 function App() {
   return (
     <div className="app-bg h-screen flex items-center justify-center">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/create-session" element={<CreateSession/>} />
+        <Route path="/sender" element={<Sender />} />
+        <Route path="/receiver" element={<Receiver />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
-// src/firebase.js
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDzZmT9ajBK7QXkutN2KwXjbO4J4OzVlNs",
   authDomain: "ez-drop-649e1.firebaseapp.com",
-  databaseURL: "https://ez-drop-649e1-default-rtdb.firebaseio.com",  // Add this line if missing
+  databaseURL: "https://ez-drop-649e1-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "ez-drop-649e1",
   storageBucket: "ez-drop-649e1.appspot.com",
   messagingSenderId: "548892944690",
   appId: "1:548892944690:web:84b914fedd90faa0d7cf2b",
-  measurementId: "G-87W3LKRWYN"
+  measurementId: "G-87W3LKRWYN",
 };
 
-// Initialize Firebase
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
 
-export { database };
+// Initialize and export Realtime Database instance
+export const database = getDatabase(app);
+
